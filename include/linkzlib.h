09fae9be1c08f927f20ed914c20a2380469d4241
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2014-12-30 16:47:51 +0800

    add zlib link header file
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

---
+++ include/linkzlib.h
@@ -0,0 +1,57 @@
+/*  Before using this project, You must import gnu_vcxx.props to your project first,
+  and set $(GNUDir) to the directories.
+    See gnu_vcxx.props.
+
+    You must add $(GNUExecutablePath) to $(ExecutablePath), add $(GNUIncludePath) to
+  $(IncludePath), add $(GNULibraryPath) to $(LibraryPath), add $(GNUSourcePath) to
+  $(SourcePath), and add $(GNUincludeDir);$(GNUinclude_etcDir) to $(AdditionalIncludeDirectories).
+
+    You can add the following configuration to vcxproj file or import $(GNUetcglobalDir)env.props
+  to your vcxproj file after <PropertyGroup Label="UserMacros" />.
+
+  <PropertyGroup>
+    <ExecutablePath>$(GNUExecutablePath);$(ExecutablePath)</ExecutablePath>
+    <IncludePath>$(GNUIncludePath);$(IncludePath)</IncludePath>
+    <LibraryPath>$(GNULibraryPath);$(LibraryPath)</LibraryPath>
+    <SourcePath>$(GNUSourcePath);$(SourcePath)</SourcePath>
+  </PropertyGroup>
+  <ItemDefinitionGroup>
+    <ClCompile>
+      <AdditionalIncludeDirectories>$(GNUincludeDir);$(GNUinclude_etcDir);
+        %(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+    </ClCompile>
+  </ItemDefinitionGroup>
+
+    If you want to use GNU projects as DLLs, defined USING_GNU_DLL, or you can defined
+  USING_ALL_DLL to use GNU and LOCAL projects as DLLs.
+    If you want to use all GNU projects in ONE DLL, please set $(GNUBuildOneDLL) to true
+  in gnu_vcxx.props file, and rebuild all GNU projects.
+*/
+
+#ifndef _ZLIB_LINK_LIB_H_
+#define _ZLIB_LINK_LIB_H_
+
+#ifdef __GNU_PROJECT_NAME__
+#  undef __GNU_PROJECT_NAME__
+#endif
+#define __GNU_PROJECT_NAME__        zlib
+
+
+#if defined ZLIB_WINAPI && !defined _M_X64
+/*  This zlib library donot use WINAPI/WINAPIV calling convention, if you
+  define ZLIB_WINAPI, undefine it. */
+/*  In AMD64, there is only one calling convention is __cdecl. */
+#  error zlib library do not use WINAPI/WINAPIV calling convention.
+#endif
+
+
+#if defined(USING_GNU_DLL) || defined(USING_ALL_DLL) || defined(USING_zlib_DLL)
+#  include "_linkdynamic"
+#else
+#  include "_linkstatic"
+#endif
+
+
+#undef __GNU_PROJECT_NAME__
+
+#endif /* _ZLIB_LINK_LIB_H_ */
