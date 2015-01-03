3d9189e92dd22b2f47dce25f2b494da0fdd0df8c
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2015-01-03 17:36:43 +0800

    add xmlrpc-c link header file
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

---
+++ include/linkxmlrpc_util.h
@@ -0,0 +1,50 @@
+/*  Before using this project, You must import gnu_vcxx.props to your project first,
+  and set $(GNUDir) to the directories.
+    See gnu_vcxx.props.
+
+    You must add $(GNUExecutablePath) to $(ExecutablePath), add $(GNUIncludePath) to
+  $(IncludePath), add $(GNULibraryPath) to $(LibraryPath), add $(GNUSourcePath) to
+  $(SourcePath), and add $(GNUincludeDir);$(GNUinclude_etcDir) to $(AdditionalIncludeDirectories).
+
+    You can add the following configuration to vcxproj file or import $(GNUetcglobalDir)env.props
+  to your vcxproj file after <PropertyGroup Label="UserMacros" />.
+
+  <PropertyGroup>
+    <ExecutablePath>$(GNUExecutablePath);$(ExecutablePath)</ExecutablePath>
+    <IncludePath>$(GNUIncludePath);$(IncludePath)</IncludePath>
+    <LibraryPath>$(GNULibraryPath);$(LibraryPath)</LibraryPath>
+    <SourcePath>$(GNUSourcePath);$(SourcePath)</SourcePath>
+  </PropertyGroup>
+  <ItemDefinitionGroup>
+    <ClCompile>
+      <AdditionalIncludeDirectories>$(GNUincludeDir);$(GNUinclude_etcDir);
+        %(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
+    </ClCompile>
+  </ItemDefinitionGroup>
+
+    If you want to use GNU projects as DLLs, defined USING_GNU_DLL, or you can defined
+  USING_ALL_DLL to use GNU and LOCAL projects as DLLs.
+    If you want to use all GNU projects in ONE DLL, please set $(GNUBuildOneDLL) to true
+  in gnu_vcxx.props file, and rebuild all GNU projects.
+*/
+
+#ifndef _XMLRPC_UTIL_LINK_LIB_H_
+#define _XMLRPC_UTIL_LINK_LIB_H_
+
+#ifdef __GNU_PROJECT_NAME__
+#  undef __GNU_PROJECT_NAME__
+#endif
+#define __GNU_PROJECT_NAME__        libxmlrpc_util
+
+
+#if defined(USING_GNU_DLL) || defined(USING_ALL_DLL) || defined(USING_xmlrpc_util_DLL)
+#  include "_linkdynamic"
+#else
+#  include "_linkstatic"
+#  pragma comment(lib, "ws2_32.lib")
+#endif
+
+
+#undef __GNU_PROJECT_NAME__
+
+#endif /* _XMLRPC_UTIL_LINK_LIB_H_ */
