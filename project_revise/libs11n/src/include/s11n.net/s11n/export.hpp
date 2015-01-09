c146569a74d40a584fdb4af2e7e6dbedef3bb4b1
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2015-01-09 22:18:49 +0800

    add fix s11n source files
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

---
+++ project_revise/libs11n/src/include/s11n.net/s11n/export.hpp
@@ -0,0 +1,37 @@
+#ifndef s11n_net_S11N_EXPORT_HPP_INCLUDED
+#define s11n_net_S11N_EXPORT_HPP_INCLUDED
+/**********************************************************************
+This file defines some macros for "exporting" symbols when the code is
+compiled as a shared library (DLL). Windows platforms, and possibly
+others, need to be told exactly what symbols are to be exported.
+
+S11N_EXPORT_API should be used when declaring a class which needs to be
+visible from outside the DLL:
+
+  class S11N_EXPORT_API my_class { ... };
+
+Free functions:
+
+  S11N_EXPORT_API int my_func() { ... }
+
+
+**********************************************************************/
+
+#ifdef WIN32
+#  if defined(USING_GNU_DLL) || defined(USING_ALL_DLL)
+#    if !defined(USING_s11n_DLL)
+#      define USING_s11n_DLL
+#    endif
+#  endif
+// #warning "Exporting Windows-style!"
+#  ifdef S11N_EXPORTS
+#    define S11N_EXPORT_API __declspec(dllexport)
+#  elif defined (USING_s11n_DLL)
+#    define S11N_EXPORT_API __declspec(dllimport)
+#  else
+#    define S11N_EXPORT_API
+#  endif
+#else
+#  define S11N_EXPORT_API
+#endif
+#endif // s11n_net_S11N_EXPORT_HPP_INCLUDED
